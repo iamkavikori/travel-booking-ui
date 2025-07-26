0cb941d4380c2bba2e360fb74ee9b017d811ef7e
Kavita <222854326+iamkavikori@users.noreply.github.com>
2025-07-27 02:14:53 +0530

    script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById('predictBtn').addEventListener('click', async () => {
     let duration = parseInt(document.getElementById('duration').value) || 200;
     let stops = parseInt(document.getElementById('stops').value) || 1;
 
-    let response = await fetch("https://travel-api-1zx0.onrender.com", {
+    let response = await fetch("https://travel-api-1zx0.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ duration: duration, stops: stops })
