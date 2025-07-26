e1d1fff4d781e01fc597310f9db44a8e2ae59602
Kavita <222854326+iamkavikori@users.noreply.github.com>
2025-07-27 01:58:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById('predictBtn').addEventListener('click', async () => {
+    let duration = parseInt(document.getElementById('duration').value) || 200;
+    let stops = parseInt(document.getElementById('stops').value) || 1;
+
+    let response = await fetch("https://travel-api-1zx0.onrender.com", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ duration: duration, stops: stops })
+    });
+
+    let result = await response.json();
+    document.getElementById('result').innerText = `💰 Estimated Price: ₹ ${result.predicted_price.toFixed(2)}`;
+});
